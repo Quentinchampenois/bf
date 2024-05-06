5fb2f89ecf4175de0aa888d8babd53f5004aec34
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-05-07 01:06:51 +0200

    feat: Add promptui

--- brainflood.go
+++ brainflood.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pelletier/go-toml/v2"
+	"log"
+	"os"
+)
 
 const filename = ".brainflood"
 
@@ -25,4 +30,39 @@ func (b Brainflood) GetInfo() {
 	fmt.Println(b.Global.Tags)
 	fmt.Println(b.Global.License)
 	fmt.Println(b.Global.Author)
+}
+
+func Projects() {
+	entries, err := os.ReadDir(projectsDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		if !e.IsDir() {
+			continue
+		}
+
+		filepath := fmt.Sprintf("%s/%s/%s", projectsDir, e.Name(), filename)
+		file, err := os.ReadFile(filepath)
+		if err != nil {
+			continue
+		}
+
+		if len(file) == 0 {
+			fmt.Println(fmt.Sprintf("%s : File is empty", filepath))
+			continue
+		}
+
+		brainflood := Brainflood{
+			Path: filepath,
+		}
+		err = toml.Unmarshal(file, &brainflood)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(2)
+		}
+
+		brainflood.GetInfo()
+	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/manifoldco/promptui"
 	"github.com/pelletier/go-toml/v2"
+	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 )
@@ -11,36 +13,83 @@ var homeDir = os.Getenv("HOME")
 var projectsDir = fmt.Sprintf("%s/Documents/local", homeDir)
 
 func main() {
-	entries, err := os.ReadDir(projectsDir)
-	if err != nil {
-		log.Fatal(err)
+	fmt.Println(homeDir)
+	fmt.Println(projectsDir)
+
+	app := &cli.App{
+		Name:  "new",
+		Usage: "create a new brainflood project",
+		Action: func(*cli.Context) error {
+			currentPath, _ := os.Getwd()
+
+			bf := Brainflood{
+				Path: currentPath,
+			}
+			prompt := promptui.Prompt{
+				Label:    "Project name",
+				Validate: Validate(3),
+			}
+
+			result, _ := prompt.Run()
+			bf.Global.Name = result
+
+			prompt = promptui.Prompt{
+				Label:    "Description",
+				Validate: Validate(8),
+			}
+
+			result, _ = prompt.Run()
+			bf.Global.Description = result
+
+			promptSelect := promptui.Select{
+				Label: "Programming language",
+				Items: []string{"Go", "Python", "JavaScript", "Ruby", "Rust", "Java", "Kotlin", "Swift", "C", "C++"},
+				Size:  10,
+			}
+
+			_, result, _ = promptSelect.Run()
+
+			bf.Global.Language = result
+
+			Writeinfile(".registry", currentPath)
+			b, err := toml.Marshal(bf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			Writeinfile(fmt.Sprintf("%s/%s", currentPath, ".brainflood"), string(b))
+			return nil
+		},
 	}
 
-	for _, e := range entries {
-		if !e.IsDir() {
-			continue
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+	//Projects()
+}
+
+func Writeinfile(filename, data string) {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if _, err := f.Write([]byte(fmt.Sprintf("%s\n", data))); err != nil {
+		fmt.Println(err.Error())
+	}
+	if err := f.Close(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func Validate(minChars int) func(s string) error {
+	return func(s string) error {
+		if len(s) < minChars {
+			return fmt.Errorf("Name must be at least %d characters", minChars)
 		}
 
-		filepath := fmt.Sprintf("%s/%s/%s", projectsDir, e.Name(), filename)
-		file, err := os.ReadFile(filepath)
-		if err != nil {
-			continue
+		if s[0] < 65 || s[0] > 90 {
+			return fmt.Errorf("Name must start with a capital letter")
 		}
-
-		if len(file) == 0 {
-			fmt.Println(fmt.Sprintf("%s : File is empty", filepath))
-			continue
-		}
-
-		brainflood := Brainflood{
-			Path: filepath,
-		}
-		err = toml.Unmarshal(file, &brainflood)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(2)
-		}
-
-		brainflood.GetInfo()
+		return nil
 	}
 }
